9c0005283059c3f95602ba2d87d75c585166b068
Nikita Raylean <nekithis@gmail.com>
2025-08-21 23:34:00 +0300

    Created interface

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ window = Tk()
 
 
 
+def create_ui(column, row, text):
+    Button(text=text, font=('Arial', 15), borderwidth=2,
+           bg='white', width=10, 
+           height=5).grid(column=column, row=row)
+    
+    
+    
+ui = (('x','',''),
+      ('','x',''),
+      ('','','x'))
+    
+for i in range(len(ui)):
+    for x in range(len(ui[i])):
+        create_ui(x, i, ui[i][x])
+
 
 
 window.mainloop()
